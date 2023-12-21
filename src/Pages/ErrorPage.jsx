1b97d8ef3d4c07856559e2763b4fe798baea7650
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-12-21 13:57:49 +0600

    added basic layout of pages

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Errorpage = () => {
+  return (
+    <div className="flex flex-col gap-y-5 items-center text-center justify-center min-h-screen">
+      <h2 className="text-9xl font-extrabold">404</h2>
+      <p className="text-3xl text-red-600 font-semibold">
+        Sorry! Page Not Found
+      </p>
+      <p className="text-xl">
+        The page you were looking for could not be found.
+      </p>
+      <Link to="/">
+        <button className="btn bg-lime-500 text-white">Go Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Errorpage;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,46 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Errorpage from './Pages/ErrorPage';
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import Contact from './Pages/Contact/Contact';
+import Dashboard from './Layout/Dashboard';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className='text-4xl'>Hello world!</div>,
+    element: <Main></Main>,
+    errorElement: <Errorpage></Errorpage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/about',
+        element: <AboutUs></AboutUs>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/dashBoard',
+        element: <Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
