ca6d949a66ebab9ec090c177e234872b3ee55eb8
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-12-22 20:04:45 +0600

    added readme file live link

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://task-manager-server-pied.vercel.app'
 })
 
 const useAxiosPublic = () => {
